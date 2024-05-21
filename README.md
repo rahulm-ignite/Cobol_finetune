d286cf746d0aec6957fd10ecddd181ec5bb0e8eb
rahulm-ignite <rahul.mahajan@ignitetech.com>
2024-05-21 10:38:42 +0530

    llama3 dataset

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Cobol_finetune
 Preparing dataset to finetune GPT 4 using COBOL dataset as required by Open AI format.
+
+
+conda create -n llama python=3.9
---
+++ jsonify_codelama.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import numpy as np
+
+
+import os
+import glob
+import json
+
+def read_cobol_files(directory):
+    # Create a pattern to match all COBOL files, assuming .cbl as the file extension
+
+    
+    cobol_files = []  # List to store the paths of COBOL files
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.cob') or file.endswith('.cbl') or file.endswith('*.cpy'):
+                cobol_files.append(os.path.join(root, file))
+    
+    # Find all files in the directory matching the pattern
+
+  #  print(cobol_files)
+    # List to store messages in the required JSON format
+    final_json = []
+    
+    for file_path in cobol_files:
+        messages = []
+        # Read the content of the file
+    #    print(file_path)
+        try :
+            with open(file_path, 'r') as file:
+                content = file.read()
+        except Exception as e:
+            pass
+        
+        length = int(round(.7*len(content),0))
+        # Append to messages as specified in the JSON structure
+        messages.append({
+            "prompt":  f"You are a COBOL code generator. 70% of the code is given to you. You need to infer the logic and complete the rest 30% of the code. The code is strictly in COBOL language. You need to output only the remaining 30% of the code \n\n {content[:length]}", "completion" : f"{content[length:]}" })
+     
+    
+
+        final_json.append(messages[0])
+    return final_json
+
+# Usage: Replace 'path_to_directory' with the actual path to the directory containing COBOL files
+directory_path = './X-COBOL'
+formatted_json = read_cobol_files(directory_path)
+# Write the list of messages to a JSON file
+output_file = 'output_messages2.txt'
+with open(output_file, 'w', encoding='utf-8') as f:
+    for j in formatted_json:
+        f.write(str(j))
+        f.write("\n")
